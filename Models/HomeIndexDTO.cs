a6b0f5377f5b95b83473534516a1d8112614796e
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-12-05 18:07:32 -0300

    style and news cards

--- Models/HomeIndexDTO.cs
+++ Models/HomeIndexDTO.cs
@@ -2,7 +2,7 @@
 {
     public class HomeIndexDTO
     {
-        public IEnumerable<ProyectoDTO> ProyectosList { get; set; }
+        public IEnumerable<ProyectoDTO>? ProyectosList { get; set; }
 
     }
 }
--- Models/ProyectoDTO.cs
+++ Models/ProyectoDTO.cs
@@ -2,10 +2,10 @@
 {
     public class ProyectoDTO
     {
-        public string  Titulo { get; set; }
-        public string Descripcion { get; set; }
-        public string ImagenURL { get; set; }
-        public string Link { get; set; }
+        public string?  Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public string? ImagenURL { get; set; }
+        public string? Link { get; set; }
 
     }
 }
