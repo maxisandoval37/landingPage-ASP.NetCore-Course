db8eb5e6c66bfb39ebf7163c29a7ac1946ec9db9
Maximiliano Sandoval <maxisandoval98@gmail.com>
2021-12-10 20:09:08 -0300

    added interface

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using landingPage.Models;
+using landingPage.Servicios;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,10 +8,12 @@ namespace landingPage.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos _repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
         {
             _logger = logger;
+            _repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
@@ -30,61 +33,9 @@ namespace landingPage.Controllers
 
         public IActionResult Season()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = _repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexDTO() { ProyectosList = proyectos };
             return View(modelo);
-        }
-
-        private List<ProyectoDTO> ObtenerProyectos()
-        {
-            return new List<ProyectoDTO>()
-            {
-                new ProyectoDTO()
-                {
-                    Titulo = "Temporada 6",
-                    Descripcion = "Rick y los demás se enteraron de la existencia de Negan cuando Daryl, " +
-                    "Abraham y Sasha fueron emboscados por una porción de los Salvadores mientras volvían a " +
-                    "Alexandría luego de alejar a la horda de caminantes que había escapado de la cantera. " +
-                    "Eventualmente, el grupo llego a la colonia Hilltop en donde Jesus confirmó la existencia " +
-                    "del tirano.",
-                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_6",
-                    ImagenURL = "/imgs/seasons/temp6.jpg"
-                },
-                new ProyectoDTO()
-                {
-                    Titulo = "Temporada 7",
-                    Descripcion = "A través de su perverso juego, Negan terminó seleccionando a Abraham " +
-                    "y lo asesinó brutalmente adelante de todos, aplastándole el cráneo con su bate de " +
-                    "béisbol hasta dejarlo prácticamente irreconocible. No contento con esto, trató " +
-                    "además de obligar a Rosita a mirar la sangre que chorreaba del arma homicida, " +
-                    "provocando finalmente que Daryl ya no pudiera contenerse y rompiera la fila para " +
-                    "tratar de golpearlo. ",
-                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_7",
-                    ImagenURL = "/imgs/seasons/temp7.jpg"
-                },
-                new ProyectoDTO()
-                {
-                    Titulo = "Temporada 8",
-                    Descripcion = "Cuando Rick y sus tropas llegaron al Santuario con la intención de " +
-                    "comenzar un enfrentamiento contra los bandidos, Negan junto a sus tenientes acudieron " +
-                    "al llamado, donde el tirano líder se sorprendió por las tropas armadas contra él y " +
-                    "escuchó atentamente como el oficial aseguró perdonar las vidas de los bandidos " +
-                    "dentro - a excepción de la del infame villano - si aceptaban rendirse.",
-                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_8",
-                    ImagenURL = "/imgs/seasons/temp8.jpg"
-                },
-                new ProyectoDTO()
-                {
-                    Titulo = "Temporada 9",
-                    Descripcion = "Durante el año y medio transcurrido desde el final de la gran guerra, " +
-                    "Negan pasó el resto de sus vidas encerrado en la improvisada cárcel que contaba " +
-                    "Alexandría y en ocasiones recibía la visita de Rick para ser informado del gran " +
-                    "avance que llevaban en sus vidas sin la presenciar del reinado de terror que el " +
-                    "infame villano había creado.",
-                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_9",
-                    ImagenURL = "/imgs/seasons/temp9.jpg"
-                },
-            };
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Servicios/RepositorioProyectos.cs
@@ -0,0 +1,64 @@
+﻿using landingPage.Models;
+
+namespace landingPage.Servicios
+{
+
+    public interface IRepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>()
+            {
+                new ProyectoDTO()
+                {
+                    Titulo = "Temporada 6",
+                    Descripcion = "Rick y los demás se enteraron de la existencia de Negan cuando Daryl, " +
+                    "Abraham y Sasha fueron emboscados por una porción de los Salvadores mientras volvían a " +
+                    "Alexandría luego de alejar a la horda de caminantes que había escapado de la cantera. " +
+                    "Eventualmente, el grupo llego a la colonia Hilltop en donde Jesus confirmó la existencia " +
+                    "del tirano.",
+                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_6",
+                    ImagenURL = "/imgs/seasons/temp6.jpg"
+                },
+                new ProyectoDTO()
+                {
+                    Titulo = "Temporada 7",
+                    Descripcion = "A través de su perverso juego, Negan terminó seleccionando a Abraham " +
+                    "y lo asesinó brutalmente adelante de todos, aplastándole el cráneo con su bate de " +
+                    "béisbol hasta dejarlo prácticamente irreconocible. No contento con esto, trató " +
+                    "además de obligar a Rosita a mirar la sangre que chorreaba del arma homicida, " +
+                    "provocando finalmente que Daryl ya no pudiera contenerse y rompiera la fila para " +
+                    "tratar de golpearlo. ",
+                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_7",
+                    ImagenURL = "/imgs/seasons/temp7.jpg"
+                },
+                new ProyectoDTO()
+                {
+                    Titulo = "Temporada 8",
+                    Descripcion = "Cuando Rick y sus tropas llegaron al Santuario con la intención de " +
+                    "comenzar un enfrentamiento contra los bandidos, Negan junto a sus tenientes acudieron " +
+                    "al llamado, donde el tirano líder se sorprendió por las tropas armadas contra él y " +
+                    "escuchó atentamente como el oficial aseguró perdonar las vidas de los bandidos " +
+                    "dentro - a excepción de la del infame villano - si aceptaban rendirse.",
+                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_8",
+                    ImagenURL = "/imgs/seasons/temp8.jpg"
+                },
+                new ProyectoDTO()
+                {
+                    Titulo = "Temporada 9",
+                    Descripcion = "Durante el año y medio transcurrido desde el final de la gran guerra, " +
+                    "Negan pasó el resto de sus vidas encerrado en la improvisada cárcel que contaba " +
+                    "Alexandría y en ocasiones recibía la visita de Rick para ser informado del gran " +
+                    "avance que llevaban en sus vidas sin la presenciar del reinado de terror que el " +
+                    "infame villano había creado.",
+                    Link = "https://thewalkingdead.fandom.com/es/wiki/Temporada_9",
+                    ImagenURL = "/imgs/seasons/temp9.jpg"
+                },
+            };
+        }
+    }
+}
